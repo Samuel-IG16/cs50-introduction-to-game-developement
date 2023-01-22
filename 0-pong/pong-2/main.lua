afeef8d3d85383ce7e2993eab77ca5b1a6d0cc07
Igbinijesu Samuel <igbinijesusamuel@gmail.com>
2023-01-22 20:04:24 +0100

    Add the rectangle update

--- 0-pong/pong-2/main.lua
+++ 0-pong/pong-2/main.lua
@@ -16,6 +16,8 @@ function love.load()
       resizable = true,
       vsync = true
   })
+  smallfont = love.graphics.newFont('font.ttf', 8)
+  love.graphics.setFont(smallfont)
 end
 --[[
     Keyboard handling, called by love2D each frame; passes in the key we pressed so we can access it
@@ -30,6 +32,10 @@ end
 ]]
 function love.draw()
   push:apply('start')
-  love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+  --love.graphics.clear(40, 45, 52, 255)
+  love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+  love.graphics.rectangle('fill', 10, 30, 5, 20)
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, VIRTUAL_HEIGHT - 50, 5, 20)
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
   push:apply('end')
 end
